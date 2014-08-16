199a1c05c767ffbeb8f16aa37e4ce1f1c12bd943
root <root@kaali.(none)>
2014-08-16 16:51:32 +0530

    Stage1-RDD: Incomplete: Acceesstokenrefresh gcs error still persists

--- CloudStorage/storage_connection.py
+++ CloudStorage/storage_connection.py
@@ -21,5 +21,30 @@ LOCAL_FILE = 'file'
 # the .boto file or as environment variables.
 CLIENT_ID = '690199257268-nmjkfa8d4jdb6mnfllkqsq6a7mc62v05.apps.googleusercontent.com'
 CLIENT_SECRET = '5IR9oQ3L1j4WsRnwxL7I6vXA'
-gcs_oauth2_boto_plugin.SetFallbackClientIdAndSecret(CLIENT_ID, CLIENT_SECRET)
+BUCKET_NAME = 'testing-stage1-rdd'
+PROJECT_ID = 'project-amoeba-testing'
+
+
+
+def create_bucket():
+	gcs_oauth2_boto_plugin.SetFallbackClientIdAndSecret(CLIENT_ID, CLIENT_SECRET)
+	uri = boto.storage_uri(BUCKET_NAME, GOOGLE_STORAGE)
+	# Try to create the bucket.
+	try:
+		header_values = {"x-goog-project-id": PROJECT_ID}
+		uri.create_bucket(headers=header_values)
+		print 'Successfully created bucket "%s"' % name
+	except boto.exception.StorageCreateError, e:
+		print 'Failed to create bucket:', e
+	except gcs_oauth2_boto_plugin.oauth2_client.AccessTokenRefreshError as e:
+		print "invalid client:", e
+
+
+
+
+
+
+if __name__ == "__main__":
+	create_bucket()
+
 
