1c4e8eb2224983394f0dfaee2d03a5fab59f135b
k <k@kaali.(none)>
2014-09-14 18:48:23 +0530

    adding proxy cheking scripts and storing in redis

--- proxies/proxy_health_checker.py
+++ proxies/proxy_health_checker.py
@@ -1,20 +1,72 @@
+#!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
 
 import sys
 import os
-parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(parent)
-from CloudStorage import RDS
+import urllib2
+import urllib
+import socket
+from redis_storage import RedisProxy
 
 class ProxyHealthCheck:
-
+	"""
+	This class checks whether the proxies stored in redis database are working fine or not
+	This is done by fetching the proxies with the status healthy from the redis 
+	The proxies which arent healthy will then have their status changed as unhealthy
+	"""
 	def __init__(self):
-		RDS_instance = RDS()
-		self.healthy_proxies = RDS_instance.healthy_proxies()
-		self.unhealthy_proxies = RDS_instance.unhealthy_proxies()
+		self.instance = RedisProxy()	
+		self.healthy_proxies = self.instance.healthy_proxies()
+		self.unhealthy_proxies = self.instance.unhealthy_proxies()
 
-		print self.healthy_proxies
-		print self.unhealthy_proxies
+	def is_bad_proxy(self, proxy_dict):
+		ip = "{ip}:{port_number}".format(ip=proxy_dict.get("ip"), port_number=proxy_dict.get("port"))
+		print ip
+		try:
+			proxy_handler = urllib2.ProxyHandler({'http': ip})
+			opener = urllib2.build_opener(proxy_handler)
+			opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+			urllib2.install_opener(opener)
+			req=urllib2.Request('http://www.google.com')  # change the URL to test here
+			sock=urllib2.urlopen(req)
+		
+		except urllib2.HTTPError, e:
+			print 'Error code: ', e.code
+			return e.code
+    
+    		except Exception, detail:
+			print "ERROR:", detail
+			return True
+		return False
 
+	def a_check_proxy(self):
+		socket.setdefaulttimeout(120)
+		
+		# two sample proxy IPs
+		for currentProxy in self.unhealthy_proxies:
+			if self.is_bad_proxy(currentProxy):
+				print "Bad Proxy %s" % (currentProxy.get("ip"))
+				self.instance.update_status(currentProxy.get("ip"), "unhealthy")
+			else:
+				print "%s is working" % (currentProxy.get("ip"))
+				self.instance.update_status(currentProxy.get("ip"), "healthy")
 
+	def b_check_proxy(self):
+		for proxy_dict in self.unhealthy_proxies:
+			ip = "http://{ip}:{port_number}".format(ip=proxy_dict.get("ip"), port_number=proxy_dict.get("port"))
+			print ip
+			
+			try:
+				urllib.urlopen("http://www.google.com", proxies={"http": ip})
+			
+			except IOError:
+				print "Connection error! (Check proxy)"
+				self.instance.update_status(currentProxy.get("ip"), "unhealthy")
+			
+			else:
+				self.instance.update_status(currentProxy.get("ip"), "healthy")
+				print "All was fine"
+if __name__ == '__main__':
+	instance = ProxyHealthCheck()
+	instance.b_check_proxy()
